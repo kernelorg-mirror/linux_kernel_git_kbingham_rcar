4bfa876546a6e29a1ea18e65c5f1fd586fb72cb2
Hans Verkuil <hans.verkuil@cisco.com>
2015-04-21 14:58:53 +0200

    vb2: Add helper function to queue request-specific buffer.
    The vb2_qbuf_request() function will queue any buffers for the given request
    that are in state PREPARED.
    
    Useful when drivers have to implement the req_queue callback.
    
    Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>

--- drivers/media/v4l2-core/videobuf2-v4l2.c
+++ drivers/media/v4l2-core/videobuf2-v4l2.c
@@ -113,6 +113,9 @@ static int __set_timestamp(struct vb2_buffer *vb, const void *pb)
 	struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
 	struct vb2_queue *q = vb->vb2_queue;
 
+	if (!pb)
+		return 0;
+
 	if (q->is_output) {
 		/*
 		 * For output buffers copy the timestamp if needed,
@@ -187,6 +190,9 @@ static int __fill_v4l2_buffer(struct vb2_buffer *vb, void *pb)
 	struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
 	struct vb2_queue *q = vb->vb2_queue;
 	unsigned int plane;
+
+	if (!pb)
+		return 0;
 
 	/* Copy back data such as timestamp, flags, etc. */
 	b->index = vb->index;
@@ -577,6 +583,31 @@ int vb2_qbuf(struct vb2_queue *q, struct v4l2_buffer *b)
 	return vb2_internal_qbuf(q, b);
 }
 EXPORT_SYMBOL_GPL(vb2_qbuf);
+
+int vb2_qbuf_request(struct vb2_queue *q, u16 request, struct vb2_buffer **p_buf)
+{
+	unsigned int buffer;
+
+	for (buffer = 0; buffer < q->num_buffers; buffer++) {
+		struct vb2_buffer *vb = q->bufs[buffer];
+		struct vb2_v4l2_buffer *vbuf = to_vb2_v4l2_buffer(vb);
+
+		if (vbuf->request == request &&
+		    vb->state == VB2_BUF_STATE_PREPARED) {
+			if (p_buf)
+				*p_buf = vb;
+			/*
+			 * The buffer has already been prepared so we can skip
+			 * the vb2_queue_or_prepare_buf() call in vb2_qbuf() and
+			 * call the core function directly.
+			 */
+			return vb2_core_qbuf(q, vb->index, NULL);
+		}
+	}
+
+	return -ENOENT;
+}
+EXPORT_SYMBOL_GPL(vb2_qbuf_request);
 
 static int vb2_internal_dqbuf(struct vb2_queue *q, struct v4l2_buffer *b,
 		bool nonblocking)
