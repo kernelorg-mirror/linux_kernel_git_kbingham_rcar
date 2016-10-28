062d839742d1e08185e6964a96c5ca0b0a81310b
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2016-10-28 11:32:01 +0200

    rcar-vin: parse Gen3 OF and setup media graph
    Parse the VIN Gen3 OF graph and register all CSI-2 devices in the VIN
    group common media device. Once all CSI-2 subdevice is added to the
    common media device setup all links between them.
    
    The parsing and registering CSI-2 subdevices with the v4l2 async
    framework is a collaborative effort shared between the VIN  instances
    which are part of the group. The fist rcar-vin instance parses OF and
    finds all other VIN and CSI-2 nodes which are part of the graph. It
    stores a bit mask of all VIN instances found and handles to all CSI-2
    nodes.
    
    The bit mask is used to figure out when all VIN instances have been
    probed. Once the last VIN instance is probed this is detected and this
    instance registers all CSI-2 subdevices in its private async notifier.
    Once the .complete() callback of this notifier is called it creates the
    media controller links between all entities in the graph and register
    all video devices with the v4l2 core.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/platform/rcar-vin/rcar-vin.h
+++ drivers/media/platform/rcar-vin/rcar-vin.h
@@ -208,7 +208,8 @@ struct rvin_dev {
  *
  * @mdev:		media device which represents the group
  *
- * @lock:		protects the vin and csi members
+ * @lock:		protects the mask, vin and csi members
+ * @mask:		Mask of VIN instances found in DT
  * @vin:		VIN instances which are part of the group
  * @csi:		CSI-2 entities that are part of the group
  */
@@ -218,6 +219,7 @@ struct rvin_group {
 	struct media_device mdev;
 
 	struct mutex lock;
+	unsigned long mask;
 	struct rvin_dev *vin[RCAR_VIN_NUM];
 	struct rvin_graph_entity csi[RVIN_CSI_MAX];
 };
