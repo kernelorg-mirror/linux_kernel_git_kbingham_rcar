cd06a8e5d5d0c7088fa59dc86ab0190ce3552754
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2017-05-24 00:19:56 +0100

    v4l2-async: Match parent devices
    Devices supporting multiple endpoints on a single device node must set
    their subdevice fwnode to the endpoint to allow distinct comparisons.
    
    Adapt the match_fwnode call to compare against the provided fwnodes
    first, but to also perform a cross reference comparison against the
    parent fwnodes of each other.
    
    This allows notifiers to pass the endpoint for comparison and still
    support existing subdevices which store their default parent device
    node.
    
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/v4l2-core/v4l2-async.c
+++ drivers/media/v4l2-core/v4l2-async.c
@@ -41,14 +41,34 @@ static bool match_devname(struct v4l2_subdev *sd,
 	return !strcmp(asd->match.device_name.name, dev_name(sd->dev));
 }
 
+/*
+ * As a measure to support drivers which have not been converted to use
+ * endpoint matching, we also find the parent devices for cross-matching.
+ *
+ * This also allows continued support for matching subdevices which will not
+ * have an endpoint themselves.
+ */
+
+static bool __match_fwnode(struct fwnode_handle *a, struct fwnode_handle *b)
+{
+	if (is_of_node(a) && is_of_node(b))
+		return !of_node_cmp(of_node_full_name(to_of_node(a)),
+				    of_node_full_name(to_of_node(b)));
+	else
+		return a == b;
+}
+
 static bool match_fwnode(struct v4l2_subdev *sd, struct v4l2_async_subdev *asd)
 {
-	if (!is_of_node(sd->fwnode) || !is_of_node(asd->match.fwnode.fwnode))
-		return sd->fwnode == asd->match.fwnode.fwnode;
+	struct fwnode_handle *asd_fwnode = asd->match.fwnode.fwnode;
+	struct fwnode_handle *sd_parent, *asd_parent;
 
-	return !of_node_cmp(of_node_full_name(to_of_node(sd->fwnode)),
-			    of_node_full_name(
-				    to_of_node(asd->match.fwnode.fwnode)));
+	sd_parent = fwnode_graph_get_port_parent(sd->fwnode);
+	asd_parent = fwnode_graph_get_port_parent(asd_fwnode);
+
+	return __match_fwnode(sd->fwnode, asd_fwnode) ||
+	       __match_fwnode(sd->fwnode, asd_parent) ||
+	       __match_fwnode(sd_parent, asd_fwnode);
 }
 
 static bool match_custom(struct v4l2_subdev *sd, struct v4l2_async_subdev *asd)
