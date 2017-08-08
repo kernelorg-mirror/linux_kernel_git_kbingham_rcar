1c15ee2ae020910f719eca8d3b3fa80b40e0fb6e
Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>
2017-08-09 01:10:03 +0200

    adv748x: csi2: only allow formats on sink pads
    Once the CSI-2 subdevice of the ADV748X becomes aware of multiplexed
    streams the format of the source pad is of no value as it carries
    multiple streams. Prepare for this by explicitly denying setting a
    format on anything but the sink pad.
    
    Signed-off-by: Niklas Söderlund <niklas.soderlund+renesas@ragnatech.se>

--- drivers/media/i2c/adv748x/adv748x-csi2.c
+++ drivers/media/i2c/adv748x/adv748x-csi2.c
@@ -182,6 +182,9 @@ static int adv748x_csi2_get_format(struct v4l2_subdev *sd,
 	struct adv748x_state *state = tx->state;
 	struct v4l2_mbus_framefmt *mbusformat;
 
+	if (sdformat->pad != ADV748X_CSI2_SINK)
+		return -EINVAL;
+
 	mbusformat = adv748x_csi2_get_pad_format(sd, cfg, sdformat->pad,
 						 sdformat->which);
 	if (!mbusformat)
@@ -204,6 +207,9 @@ static int adv748x_csi2_set_format(struct v4l2_subdev *sd,
 	struct adv748x_state *state = tx->state;
 	struct v4l2_mbus_framefmt *mbusformat;
 	int ret = 0;
+
+	if (sdformat->pad != ADV748X_CSI2_SINK)
+		return -EINVAL;
 
 	mbusformat = adv748x_csi2_get_pad_format(sd, cfg, sdformat->pad,
 						 sdformat->which);
