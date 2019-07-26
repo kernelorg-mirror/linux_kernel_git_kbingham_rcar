009b909c547696851783d0d4faa64514f48a7c66
Kieran Bingham <kieran.bingham@ideasonboard.com>
2019-07-26 14:29:19 +0100

    Revert "rcar-vin: rcar-csi2: Fix field detection"
    This reverts commit 4c3fad83e30daa43ace650535f3ab34034029e70.
    
    This topic conflicts with backporting the Virtual Channel support for 8
    camera GMSL.
    
    Simply revert this feature for the moment.
    
    Not-signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>

--- drivers/media/platform/rcar-vin/rcar-csi2.c
+++ drivers/media/platform/rcar-vin/rcar-csi2.c
@@ -482,13 +482,12 @@ static int rcsi2_calc_mbps(struct rcar_csi2 *priv, unsigned int bpp)
 	return mbps;
 }
 
-static int rcsi2_start(struct rcar_csi2 *priv, struct v4l2_subdev *nextsd)
+static int rcsi2_start(struct rcar_csi2 *priv)
 {
 	const struct rcar_csi2_format *format;
-	u32 phycnt, vcdt = 0, vcdt2 = 0, fld = 0;
+	u32 phycnt, vcdt = 0, vcdt2 = 0;
 	unsigned int i;
 	int mbps, ret;
-	v4l2_std_id std = 0;
 
 	dev_dbg(priv->dev, "Input size (%ux%u%c)\n",
 		priv->mf.width, priv->mf.height,
@@ -496,17 +495,6 @@ static int rcsi2_start(struct rcar_csi2 *priv, struct v4l2_subdev *nextsd)
 
 	/* Code is validated in set_fmt. */
 	format = rcsi2_code_to_fmt(priv->mf.code);
-
-	ret = v4l2_subdev_call(nextsd, video, g_std, &std);
-	if (ret < 0 && ret != -ENOIOCTLCMD && ret != -ENODEV)
-		return ret;
-
-	if (priv->mf.field != V4L2_FIELD_NONE) {
-		if (std & V4L2_STD_525_60)
-			fld = FLD_FLD_NUM(2);
-		else
-			fld = FLD_FLD_NUM(1);
-	}
 
 	/*
 	 * Enable all Virtual Channels.
@@ -544,6 +532,8 @@ static int rcsi2_start(struct rcar_csi2 *priv, struct v4l2_subdev *nextsd)
 		    INTEN_INT_ERRSOTHS | INTEN_INT_ERRSOTSYNCHS);
 
 	/* Configure */
+	rcsi2_write(priv, FLD_REG, FLD_FLD_NUM(2) | FLD_FLD_EN4 |
+			    FLD_FLD_EN3 | FLD_FLD_EN2 | FLD_FLD_EN);
 	rcsi2_write(priv, VCDT_REG, vcdt);
 	rcsi2_write(priv, VCDT2_REG, vcdt2);
 	/* Lanes are zero indexed. */
@@ -573,12 +563,6 @@ static int rcsi2_start(struct rcar_csi2 *priv, struct v4l2_subdev *nextsd)
 	rcsi2_write(priv, PHYCNT_REG, phycnt);
 	rcsi2_write(priv, LINKCNT_REG, LINKCNT_MONITOR_EN |
 		    LINKCNT_REG_MONI_PACT_EN | LINKCNT_ICLK_NONSTOP);
-
-	if (priv->mf.field != V4L2_FIELD_NONE)
-		rcsi2_write(priv, FLD_REG, fld | FLD_FLD_EN4 |
-			    FLD_FLD_EN3 | FLD_FLD_EN2 | FLD_FLD_EN);
-	else
-		rcsi2_write(priv, FLD_REG, 0);
 
 	rcsi2_write(priv, PHYCNT_REG, phycnt | PHYCNT_SHUTDOWNZ);
 	rcsi2_write(priv, PHYCNT_REG, phycnt | PHYCNT_SHUTDOWNZ | PHYCNT_RSTZ);
@@ -628,7 +612,7 @@ static int rcsi2_s_stream(struct v4l2_subdev *sd, int enable)
 		pm_runtime_get_sync(priv->dev);
 		reset_control_deassert(priv->rstc);
 
-		ret = rcsi2_start(priv, nextsd);
+		ret = rcsi2_start(priv);
 		if (ret) {
 			pm_runtime_put(priv->dev);
 			goto out;
