25042a1948d87b59c8e70107ba74c1b5ae53ffc2
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2020-02-12 14:09:45 +0000

    DNI: Increase REV_AMP later.
    This didn't fix my faulty camera : RDACM20(01041)

--- drivers/media/i2c/max9286.c
+++ drivers/media/i2c/max9286.c
@@ -659,10 +659,14 @@ static int max9286_notify_bound(struct v4l2_async_notifier *notifier,
 	 * All enabled sources have probed and enabled their reverse control
 	 * channels:
 	 *
+	 * - Set to 170mV (_AMP_X is assumed to be +100)
 	 * - Verify all configuration links are properly detected
 	 * - Disable auto-ack as communication on the control channel are now
 	 *   stable.
 	 */
+	max9286_write(priv, 0x3b, MAX9286_REV_TRF(1) | MAX9286_REV_AMP(70) |
+		      MAX9286_REV_AMP_X);
+
 	max9286_check_config_link(priv, priv->source_mask);
 
 	/*
@@ -1091,7 +1095,7 @@ static int max9286_setup(struct max9286_priv *priv)
 	 *   shows slightly less reliable communications.
 	 */
 	max9286_write(priv, 0x3f, MAX9286_EN_REV_CFG | MAX9286_REV_FLEN(35));
-	max9286_write(priv, 0x3b, MAX9286_REV_TRF(1) | MAX9286_REV_AMP(70) |
+	max9286_write(priv, 0x3b, MAX9286_REV_TRF(1) | MAX9286_REV_AMP(100) |
 		      MAX9286_REV_AMP_X);
 	usleep_range(2000, 2500);
 
