df07419a92e40ec9927b9abf85fe7a9c6b14e015
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2020-04-09 12:10:28 +0100

    sqaush! max9286: Remove poc_enabled workaround
    This boolean is a flag used to handle the regulator when our
    multi-max9286 workaround is in place.  It shouldn't be in the upstream
    driver, and is moved out.
    
    Signed-off-by: Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>

--- drivers/media/i2c/max9286.c
+++ drivers/media/i2c/max9286.c
@@ -149,7 +149,6 @@ struct max9286_priv {
 	struct v4l2_subdev sd;
 	struct media_pad pads[MAX9286_N_PADS];
 	struct regulator *regulator;
-	bool poc_enabled;
 
 	struct gpio_chip gpio;
 	u8 gpio_state;
@@ -992,8 +991,6 @@ static int max9286_init(struct device *dev)
 		return ret;
 	}
 
-	priv->poc_enabled = true;
-
 	ret = max9286_setup(priv);
 	if (ret) {
 		dev_err(dev, "Unable to setup max9286\n");
@@ -1025,7 +1022,6 @@ err_v4l2_register:
 	max9286_v4l2_unregister(priv);
 err_regulator:
 	regulator_disable(priv->regulator);
-	priv->poc_enabled = false;
 
 	return ret;
 }
@@ -1250,8 +1246,7 @@ static int max9286_remove(struct i2c_client *client)
 
 	max9286_v4l2_unregister(priv);
 
-	if (priv->poc_enabled)
-		regulator_disable(priv->regulator);
+	regulator_disable(priv->regulator);
 
 	gpiod_set_value_cansleep(priv->gpiod_pwdn, 0);
 
