5e7a11618ca1bdce7885adddee659dbb871cedb8
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2021-03-22 03:39:02 +0200

    drm/bridge: ti-sn65dsi86: Make connector creation optional
    Now that the driver supports the connector-related bridge operations,
    make the connector creation optional. This enables usage of the
    sn65dsi86 with the DRM bridge connector helper.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>

--- drivers/gpu/drm/bridge/ti-sn65dsi86.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi86.c
@@ -623,11 +623,6 @@ static int ti_sn_bridge_attach(struct drm_bridge *bridge,
 						   .node = NULL,
 						 };
 
-	if (flags & DRM_BRIDGE_ATTACH_NO_CONNECTOR) {
-		DRM_ERROR("Fix bridge driver to make connector optional!");
-		return -EINVAL;
-	}
-
 	pdata->aux.drm_dev = bridge->dev;
 	ret = drm_dp_aux_register(&pdata->aux);
 	if (ret < 0) {
@@ -635,9 +630,11 @@ static int ti_sn_bridge_attach(struct drm_bridge *bridge,
 		return ret;
 	}
 
-	ret = ti_sn_bridge_connector_init(pdata);
-	if (ret < 0)
-		goto err_conn_init;
+	if (!(flags & DRM_BRIDGE_ATTACH_NO_CONNECTOR)) {
+		ret = ti_sn_bridge_connector_init(pdata);
+		if (ret < 0)
+			goto err_conn_init;
+	}
 
 	/*
 	 * TODO: ideally finding host resource and dsi dev registration needs
