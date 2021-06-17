daemory/rcar

debugfs: Extend debugfs regset to support register decoding
Allow regsets to provide a function to present decoded information on
register usage. This allows a static function to be passed the value
read from the register and present the information in a human readable
form.

Signed-off-by: Kieran Bingham <kieran@bingham.xyz>

fs/debugfs/file.c
@@ -1027,10 +1027,17 @@ void debugfs_print_regs32(struct seq_file *s, const struct debugfs_reg32 *regs,
 	int i;
 
 	for (i = 0; i < nregs; i++, regs++) {
+		u32 value = readl(base + regs->offset);
 		if (prefix)
 			seq_printf(s, "%s", prefix);
-		seq_printf(s, "%s = 0x%08x\n", regs->name,
-			   readl(base + regs->offset));
+
+		seq_printf(s, "%s = 0x%08x", regs->name, value);
+
+		if (regs->decode_reg)
+			regs->decode_reg(s, value);
+
+		seq_printf(s, "\n");
+
 		if (seq_has_overflowed(s))
 			break;
 	}

kernelorg-mirror/linux_kernel_git_kbingham_rcar

fs/debugfs/file.c
@@ -1107,10 +1107,17 @@ void debugfs_print_regs32(struct seq_file *s, const struct debugfs_reg32 *regs,
 	int i;
 
 	for (i = 0; i < nregs; i++, regs++) {
+		u32 value = readl(base + regs->offset);
 		if (prefix)
 			seq_printf(s, "%s", prefix);
-		seq_printf(s, "%s = 0x%08x\n", regs->name,
-			   readl(base + regs->offset));
+
+		seq_printf(s, "%s = 0x%08x", regs->name, value);
+
+		if (regs->decode_reg)
+			regs->decode_reg(s, value);
+
+		seq_puts(s, "\n");
+
 		if (seq_has_overflowed(s))
 			break;
 	}