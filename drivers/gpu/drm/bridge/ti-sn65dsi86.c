4fe17480f1aee0400b55dfb91d233bf3848adf45
Kieran Bingham <kieran.bingham+renesas@ideasonboard.com>
2022-06-22 00:49:24 +0100

    squash! drm/bridge: ti-sn65dsi86: Implement bridge connector operations
    ---
    Changes since v4:
     - Add missing drm/drm_edid include
    
    Reviewed-by: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/bridge/ti-sn65dsi86.c
+++ drivers/gpu/drm/bridge/ti-sn65dsi86.c
@@ -29,6 +29,7 @@
 #include <drm/drm_atomic_helper.h>
 #include <drm/drm_bridge.h>
 #include <drm/drm_bridge_connector.h>
+#include <drm/drm_edid.h>
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_of.h>
 #include <drm/drm_panel.h>
